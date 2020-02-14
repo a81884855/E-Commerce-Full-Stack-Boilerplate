693f8aff07849b4f2a8d0f02935c792685631648
Gary <garyguan0713@gmail.com>
2020-02-14 11:07:48 -0800

    init slider area component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./web/Header";
+import SliderArea from "./web/SliderArea";
 
 function App() {
   return (
     <>
       <Header />
+      <SliderArea />
       <div className="App">Hello</div>
     </>
   );
---
+++ src/web/SliderArea/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SliderArea = () => {
+  return <div className="slider-area"></div>;
+};
+
+export default SliderArea;
