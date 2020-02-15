c7ccb377d4a9d2cb464057282d730193a105ec2e
Gary <garyguan0713@gmail.com>
2020-02-15 00:01:40 -0800

    Add productArea

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./web/Header";
 import SliderArea from "./web/Slider";
+import ProductArea from "./web/ProductArea";
 
 function App() {
   return (
     <>
       <Header />
       <SliderArea />
+      <ProductArea />
     </>
   );
 }
