1942d0f01bc234eac8ae9abe77729e7018657eeb
Gary <garyguan0713@gmail.com>
2020-02-25 14:18:38 -0800

    implement filter widget component in sidebar

---
+++ src/web/Shop/Sidebar/Widget.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Widget = () => {
+  return (
+    <>
+      <h2 className="mt-40 mb-40">Categories</h2>
+      <div className="widget">
+        <div>
+          <a href="/">New</a>
+          <span>(16)</span>
+        </div>
+        <div>
+          <a href="/">Tops</a>
+          <span>(16)</span>
+        </div>
+        <div>
+          <a href="/">Jackets</a> <span>(16)</span>
+        </div>
+        <div>
+          <a href="/">Swim</a> <span>(16)</span>
+        </div>
+        <div>
+          <a href="/">Beauty</a> <span>(16)</span>
+        </div>
+        <div>
+          <a href="/">Shoes</a> <span>(16)</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Widget;
---
+++ src/web/Shop/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Widget from "./Widget";
+import Filter from "./Filter";
+
+const SideBar = () => {
+  return (
+    <div>
+      <Widget />
+      <Filter />
+    </div>
+  );
+};
+
+export default SideBar;
