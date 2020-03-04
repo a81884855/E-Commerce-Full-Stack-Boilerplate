985cd861ee3e4a606bcb7831136956c0e2aecf27
Gary <garyguan0713@gmail.com>
2020-03-04 15:54:38 -0800

    init shopTopBar

---
+++ src/web/Shop/ProductArea/shopTopBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+export const shopTopBar = () => {
+  return (
+    <Row className="shop-top-bar">
+      <Col className="bar-left"></Col>
+      <Col className="bar-right"></Col>
+    </Row>
+  );
+};
