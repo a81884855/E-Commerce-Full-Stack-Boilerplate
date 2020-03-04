b39be3a509cc44ab8077f58d239bf774cdd6fccd
Gary <garyguan0713@gmail.com>
2020-03-04 15:53:43 -0800

    add the first in the end of products

--- src/web/HomePage/ProductArea/index.js
+++ src/web/HomePage/ProductArea/index.js
@@ -16,7 +16,10 @@ const Index = () => {
   return (
     <>
       <div className="productArea pt-120 pb-120 ps-2vw">
-        <ProductArea products={newArrival} title="New Arrival" />
+        <ProductArea
+          products={[...newArrival, newArrival[0]]}
+          title="New Arrival"
+        />
       </div>
 
       <div className="productArea pt-70 pb-120 ps-2vw">
