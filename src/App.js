bcbdd6a5388b28291863f3ce4e1e2697bbd7b7be
Gary <garyguan0713@gmail.com>
2020-03-06 15:19:24 -0800

    implement product Provider

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./web/HomePage";
 import Header from "./web/Header";
 import Shop from "./web/Shop";
+import { Provider as ProductProvider } from "./context/ProductContext";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/Shop">
-          <Shop />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <ProductProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/Shop">
+            <Shop />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </ProductProvider>
   );
 }
 
