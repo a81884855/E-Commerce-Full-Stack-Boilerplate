d827f1e364894deacf66e7c3e443dbd2906a6fcf
Gary <garyguan0713@gmail.com>
2020-03-31 18:17:43 -0700

    implement payment container

--- src/web/Header/index.js
+++ src/web/Header/index.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Navbar, Nav, Col } from "react-bootstrap";
 import { AiOutlineShopping } from "react-icons/ai";
 import { IoIosSearch, IoMdMenu, IoMdHeartEmpty } from "react-icons/io";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import mastercard from "../../assets/mastercard.png";
+import visa from "../../assets/visa.png";
+import amex from "../../assets/amex.png";
+import paypal from "../../assets/paypal.png";
+import SocialLink from "../../Components/SocialLink";
 
 export default function Header() {
+  const [open, setOpen] = useState(true);
   return (
     <div className="nav-container ps-2vw">
       <Col xl={1} lg={1} md={6} sm={6} xs={6}>
@@ -22,18 +29,6 @@ export default function Header() {
           <Nav.Link href="#pricing">Accessirues</Nav.Link>
           <Nav.Link href="#pricing">Sales</Nav.Link>
           <Nav.Link href="#pricing">About US</Nav.Link>
-
-          {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown> */}
         </Nav>
       </Col>
 
@@ -54,10 +49,49 @@ export default function Header() {
         <a href="/">
           <IoIosSearch />
         </a>
-        <a href="/">
+        <a onClick={() => setOpen(true)}>
           <IoMdMenu />
         </a>
       </Col>
+      <SwipeableDrawer
+        anchor="right"
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+      >
+        {sideList()}
+      </SwipeableDrawer>
     </div>
   );
 }
+
+const sideList = () => (
+  <div className="sideList" role="presentation">
+    <div className="list pb-40">
+      <div>About Me</div>
+      <div>Help Center</div>
+      <div>Collection</div>
+      <div>Blog</div>
+      <div>New Look</div>
+    </div>
+
+    <div className="payment">
+      <div className="payment-container">
+        <img src={visa} className="payment-logo" alt="visa" />
+        <img src={mastercard} className="payment-logo" alt="mastercard" />
+        <img src={amex} className="payment-logo" alt="amex" />
+        <img src={paypal} className="payment-logo" alt="paypal" />
+      </div>
+
+      <p>
+        Malesuada mauris. Sed scelerisque felis vel rutrum dignissim. Vivamus
+        euismod mi at gravida tincidunt. Vivamus sit amet luctus nisi. Vivamus
+        pretium purus eget ante placerat malesuada.
+      </p>
+    </div>
+
+    <div className="social-link">
+      <SocialLink facebook twitter googlePlus youtube />
+    </div>
+  </div>
+);
